83b016a4219516ac376c0f175cf4f6316cb25e2e
root <root@kali.kali>
2025-02-19 21:41:51 +0100

    NBIA downloader for cancer images

---
+++ src/Pipeline/Data/NBIA_download.py
@@ -0,0 +1,18 @@
+from nbiatoolkit import NBIAClient
+import os
+
+with NBIAClient() as client:
+    collection_name = 'Lung-PET-CT-Dx'
+    dir_name = "manifest-160866918333/Lung-PET-CT-Dx/"
+   
+    os.makedirs(dir_name, exist_ok=True)
+
+    # Obtén la lista de series en la colección
+    series_list = client.getSeries(Collection=collection_name)
+    
+    # Descarga cada serie en la colección
+    for series in series_list:
+        client.downloadSeries(series['SeriesInstanceUID'], dir_name)
+        print("Descargado") 
+
+    print(f'Colección {collection_name} descargada en: {dir_name}')
