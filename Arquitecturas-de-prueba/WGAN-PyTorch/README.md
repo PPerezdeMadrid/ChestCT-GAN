581e9527c1dc2655de81f686ae631aa1b84fa68c
Paloma Pérez de Madrid <paloma.perezmadridlaguna@usp.ceu.es>
2025-04-21 16:06:36 +0200

    readme y eval model

--- Arquitecturas-de-prueba/WGAN-PyTorch/README.md
+++ Arquitecturas-de-prueba/WGAN-PyTorch/README.md
@@ -1,2 +1,131 @@
-train_gp: train w/ gradient penalty
-train_wp: train w/ weight clipping (menos efectivo según https://arxiv.org/abs/1704.00028)
+# Implementación de WGAN-GP
+
+Este apartado contiene la implementación de una Red Generativa Antagónica de Wasserstein con Penalización por Gradiente (WGAN-GP) para la generación de imágenes. El modelo puede entrenarse utilizando penalización por gradiente o recorte de pesos. El código incluye varios scripts para entrenar, generar y evaluar imágenes, así como para gestionar las configuraciones y los registros de entrenamiento.
+
+## Requisitos
+
+Asegúrate de tener instalados los siguientes paquetes de Python:
+
+- `torch`
+- `torchvision`
+- `numpy`
+- `matplotlib`
+- `pandas`
+- `scikit-image`
+
+Puedes instalarlos utilizando `pip`:
+
+```bash
+pip install torch torchvision numpy matplotlib pandas scikit-image
+```
+
+## Estructura del Proyecto
+
+```plaintext
+├── config.json                # Configuración de hiperparámetros y rutas
+├── training_log_wgan.csv      # Registros de entrenamiento
+├── generate.py                # Generación de imágenes
+├── eval_model.py              # Evaluación del modelo
+├── graphLogs.py               # Graficar registros de entrenamiento
+├── model.py                   # Definición del modelo WGAN-GP
+└── utils.py                   # Funciones utilitarias
+```
+
+## Descripción de los Scripts
+
+### `generate.py`
+
+Este script se usa para generar imágenes a partir de un modelo entrenado. Permite cargar un modelo preentrenado, especificar la cantidad de imágenes a generar y, opcionalmente, comparar las imágenes generadas con las reales.
+
+**Uso:**
+
+```bash
+python generate.py -load_path <ruta_checkpoint> -num_output 64 -compare
+```
+
+- `-load_path`: Ruta al checkpoint del modelo entrenado (por defecto: `./model/model.pth`)
+- `-num_output`: Número de imágenes generadas (por defecto: 64)
+- `-compare`: Opción para mostrar una comparación entre las imágenes generadas y las reales.
+
+### `eval_model.py`
+
+Este script evalúa el rendimiento del modelo utilizando varias métricas como la precisión del discriminador, la precisión del generador, SSIM, PSNR y LPIPS.
+
+**Uso:**
+
+```bash
+python eval_model.py --dataset chestct --model_name model_ChestCT.pth
+```
+
+- `--dataset`: Elige entre los datasets `"nbia"` o `"chestct"` para la evaluación.
+- `--model_name`: Nombre del checkpoint del modelo a cargar.
+
+Las métricas de evaluación incluyen:
+- **Precisión del discriminador**
+- **Precisión del generador**
+- **SSIM** (Índice de Similaridad Estructural)
+- **PSNR** (Relación de Señal a Ruido Pico)
+- **LPIPS** (Similitud Perceptual de Parche de Imagen Aprendida)
+
+### `graphLogs.py`
+
+Este script permite graficar los registros de entrenamiento guardados en `training_log_wgan.csv`, visualizando métricas como la pérdida del generador y la pérdida del discriminador a lo largo de las épocas.
+
+### `config.json`
+
+Este archivo contiene la configuración del modelo, incluidos los hiperparámetros, las rutas para guardar los modelos e imágenes generadas, y las rutas de los datasets de entrada.
+
+```json
+{
+    "params": {
+        "bsize": 128,
+        "imsize": 64,
+        "nc": 1,
+        "nz": 100,
+        "ngf": 64,
+        "ndf": 64,
+        "nepochs": 1000,
+        "lr": 0.0001,
+        "beta1": 0.5,
+        "save_epoch": 100,
+        "critic_iters": 5
+    },
+    "model": {
+        "path": "model/model_wgan",
+        "image_path": "images/images_wgan"
+    },
+    "datasets":{
+        "chestKaggle": "../../../../TFG/ChestCTKaggle/Data/",
+        "nbia": "../../src/Pipeline/Data/Data-Transformed"
+    }
+}
+```
+
+- **params**: Define los hiperparámetros del modelo (por ejemplo, `latent_dim`, `batch_size`, `num_epochs`, `learning_rate`).
+- **model**: Establece las rutas para guardar el modelo y las imágenes generadas.
+- **datasets**: Establece las rutas de los conjuntos de datos.
+
+### `training_log_wgan.csv`
+
+Este archivo CSV guarda los registros de cada época durante el entrenamiento. Incluye métricas como la pérdida del discriminador, la pérdida del generador y la distancia Wasserstein. Puedes usar `graphLogs.py` para visualizar estas métricas.
+
+### Opciones de Entrenamiento del Modelo
+
+Puedes entrenar el modelo utilizando Penalización por Gradiente o Recorte de Pesos.
+
+- **`train_gp.py`**: Entrena el modelo WGAN-GP con penalización por gradiente.
+- **`train_wp.py`**: Entrena el modelo WGAN con recorte de pesos (menos efectivo según el artículo original de WGAN).
+
+### Justificación de la Elección de `train_gp` sobre `train_wp`
+
+El uso de **penalización por gradiente** en WGAN-GP es más efectivo que el **recorte de pesos** en la mejora de la estabilidad durante el entrenamiento, como se argumenta en el artículo original de WGAN-GP (Arjovsky et al., 2017). En el artículo, se señala que el recorte de pesos puede inducir una distorsión en la función de optimización, lo que provoca que la convergencia sea más lenta y, en algunos casos, inestable. Por otro lado, la **penalización por gradiente** mejora la estabilidad del entrenamiento al suavizar el gradiente y evitar los problemas derivados de la saturación del discriminador.
+
+El uso de **WGAN-GP** ha demostrado ser más eficiente en la generación de imágenes de alta calidad y en la mejora de las métricas de evaluación, como se observa en las comparaciones de los resultados experimentales. Por lo tanto, es recomendable usar `train_gp.py` para entrenar el modelo, ya que ofrece una mejor convergencia y estabilidad en comparación con `train_wp.py`.
+
+### Comando de Ejemplo para Entrenar con Penalización por Gradiente
+
+```bash
+python train_gp.py
+```
+
+Esto entrenará el modelo utilizando los hiperparámetros especificados en `config.json` y guardará los checkpoints durante el proceso.
--- Arquitecturas-de-prueba/WGAN-PyTorch/eval_model.py
+++ Arquitecturas-de-prueba/WGAN-PyTorch/eval_model.py
@@ -1,99 +1,50 @@
-import torch, json, lpips
+import torch, json, os
+import lpips
 import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 from torchvision import models
+from torch.utils.data import DataLoader
 from torch.nn import functional as F
-from wgan import Generator 
+from wgan import Generator , Discriminator
 from PIL import Image
 import numpy as np
 from scipy.linalg import sqrtm
-from utils import get_chestct 
-from wgan import Generator, Discriminator
+from utils import get_chestct
 from skimage.metrics import structural_similarity as ssim
-import pandas as pd
-import matplotlib.pyplot as plt
-
+from scipy.linalg import sqrtm
+from scipy.stats import entropy
 
 
 def print_green(text):
     print("\033[92m" + text + "\033[0m")
 
-print_green("Evaluating model...")
-
-# leer parámetros y modelo:
 with open('config.json', 'r') as json_file:
     config = json.load(json_file)
 
-
-model_path = config["model"]["path"]
-image_path = config["model"]["image_path"]
-
-print_green("Parameters uploaded")
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print_green(f'{device} will be used.\n')
-
-
-# Cargar el modelo entrenado con weights_only=True para una mayor seguridad
-checkpoint = torch.load(f'{model_path}/model_ChestCT.pth', weights_only=True)
-params = checkpoint["params"]
-
-# Get the data
-dataloader = get_chestct(params)
-
-# Load the generator and discriminator
-netG = Generator(params).to(device)
-netD = Discriminator(params).to(device)
-
-netG.load_state_dict(checkpoint['generator'])
-netD.load_state_dict(checkpoint['discriminator'])
-
-
-# Modo evaluación
-netG.eval()
-netD.eval()
-
-
-def generate_images(generator, num_images=100, latent_size=100):
-    """Genera imágenes usando el generador."""
-    noise = torch.randn(num_images, latent_size, 1, 1, device=device)  # Generar ruido
-    with torch.no_grad():
-        fake_images = generator(noise).detach().cpu()
-
-    if fake_images.shape[1] == 1:  # Si solo tiene 1 canal
-        fake_images = fake_images.repeat(1, 3, 1, 1)  # Repetir el canal
-
-    return fake_images
-
-def preprocess_images(images, size=(299, 299)):
-    """Preprocesa imágenes para Inception-v3."""
-    preprocess = transforms.Compose([
-        transforms.Resize(size),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        transforms.Normalize(mean=[0.5], std=[0.5])  # Normalización a [-1, 1] para un solo canal
-
-    ])
+def load_model(model_path, device, model_type, model_name):
+    checkpoint = torch.load(f'{model_path}/{model_name}', map_location=device)
+    params = checkpoint['params']
+    if model_type == 'dcgan':
+        """
+        """
+    elif model_type == 'wgan':
+        netG = Generator(params).to(device)
+        netD = Discriminator(params).to(device)
+    else:
+        raise ValueError(f"Unknown model type: {model_type}")
     
-    processed_images = []
-    for img in images:
-        img = transforms.ToPILImage()(img)  # Convertir tensor a PIL Image
-        img = preprocess(img)
-        processed_images.append(img)
-    
-    return torch.stack(processed_images)
-
-""""
-##############################
-    Discriminador y Generador
-######################
-"""
+    netG.load_state_dict(checkpoint['generator'])
+    netD.load_state_dict(checkpoint['discriminator'])
+    netG.eval()
+    netD.eval()
+    return netG, netD, params
 
 
 def evaluate_models(netG, netD, dataloader, device, params):
-    # Evaluar el discriminador
     correct_discriminator = 0
     total = 0
     correct_generator = 0
+    discriminator_threshold = 0.5  # Umbral para el discriminador
 
     with torch.no_grad():
         # Evaluar el discriminador
@@ -104,7 +55,7 @@ def evaluate_models(netG, netD, dataloader, device, params):
             # Evaluar el discriminador en datos reales
             real_label_tensor = torch.full((b_size,), 1, dtype=torch.float, device=device)
             output_real = netD(real_data).view(-1)
-            correct_discriminator += ((output_real > 0.5).float() == real_label_tensor).sum().item()
+            correct_discriminator += ((output_real > discriminator_threshold).float() == real_label_tensor).sum().item()
 
             # Generar datos falsos
             noise = torch.randn(b_size, params['nz'], 1, 1, device=device)
@@ -113,182 +64,136 @@ def evaluate_models(netG, netD, dataloader, device, params):
             # Evaluar el discriminador en datos generados
             fake_label_tensor = torch.full((b_size,), 0, dtype=torch.float, device=device)
             output_fake = netD(fake_data.detach()).view(-1)
-            correct_discriminator += ((output_fake < 0.5).float() == fake_label_tensor).sum().item()
+            correct_discriminator += ((output_fake < discriminator_threshold).float() == fake_label_tensor).sum().item()
 
             total += b_size * 2  # Para datos reales y generados
 
         # Calcular precisión del discriminador
         accuracy_discriminator = correct_discriminator / total
 
-        # Evaluar la precisión del generador
-        for _ in range(100):  # Generar 100 datos de prueba
+        # Evaluar la precisión del generador con más muestras (aumentar a más de 100)
+        num_test_samples = 500  # Usar más muestras para mejorar la precisión
+        for _ in range(num_test_samples):  # Generar más datos de prueba
             noise = torch.randn(1, params['nz'], 1, 1, device=device)
             generated_data = netG(noise)
             output = netD(generated_data.detach()).view(-1)
-            if output > 0.5:
+            if output > discriminator_threshold:
                 correct_generator += 1
 
-        accuracy_generator = correct_generator / 100
+        accuracy_generator = correct_generator / num_test_samples
 
     return accuracy_discriminator, accuracy_generator
 
 
-accuracy_discriminator, accuracy_generator = evaluate_models(netG, netD, dataloader, device, params)
-
-# Imprimir --> Generador y Discriminador
-print(f"{'-' * 30}")
-print(f"{'Model Evaluation Results':^30}")
-print(f"{'-' * 30}")
-print(f"{'Discriminator Accuracy:':<20} {accuracy_discriminator * 100:.2f}%")
-print(f"{'Generator Accuracy:':<20} {accuracy_generator * 100:.2f}%")
-print(f"{'-' * 30}")
-
-
-
-"""
-#############################################  
-       Structural Similarity Index (SSIM)
- ############################################   
-"""
-
 def calculate_ssim(real_images, fake_images):
-    """Calcula el índice SSIM entre imágenes reales y generadas."""
-    real_images = real_images.squeeze().cpu().numpy()  # Eliminar el canal adicional (si es necesario)
-    fake_images = fake_images.squeeze().cpu().numpy()  # Eliminar el canal adicional (si es necesario)
-
-    # Asegurarse de que las imágenes tengan el mismo tamaño
+    real_images = real_images.squeeze().cpu().numpy()
+    fake_images = fake_images.squeeze().cpu().numpy()
     fake_images_resized = np.resize(fake_images, real_images.shape)
-
-    # Calcular SSIM para cada par de imágenes
-    ssim_values = []
-    for real, fake in zip(real_images, fake_images_resized):
-        # Aquí definimos el data_range como 2, ya que las imágenes están en [-1, 1]
-        ssim_value = ssim(real, fake, data_range=2.0)
-        ssim_values.append(ssim_value)
-
-    return np.mean(ssim_values)  # Devolver el valor promedio del SSIM
+    ssim_values = [ssim(real, fake, data_range=2.0) for real, fake in zip(real_images, fake_images_resized)]
+    return np.mean(ssim_values)
 
 def evaluate_ssim(dataloader, netG, device):
-    """Evaluar SSIM entre imágenes reales y generadas."""
-    real_images = []
-    fake_images = []
-    
+    real_images, fake_images = [], []
     with torch.no_grad():
         for real_data, _ in dataloader:
             real_data = real_data.to(device)
-
-            # Obtener imágenes reales
             real_images.append(real_data)
+            noise = torch.randn(real_data.size(0), 100, 1, 1, device=device)
+            fake_images.append(netG(noise))
+    return calculate_ssim(torch.cat(real_images), torch.cat(fake_images))
 
-            # Generar imágenes falsas
-            noise = torch.randn(real_data.size(0), 100, 1, 1, device=device)  # Ajustar según tu tamaño de latente
-            fake_data = netG(noise)
-            fake_images.append(fake_data)
-        
-    # Convertir listas a tensores
-    real_images = torch.cat(real_images, dim=0)
-    fake_images = torch.cat(fake_images, dim=0)
-    
-    # Calcular SSIM
-    ssim_score = calculate_ssim(real_images, fake_images)
-    return ssim_score
-
-# Evaluar el SSIM
-ssim_score = evaluate_ssim(dataloader, netG, device)
-# Imprimir --> SSIM 
-print(f"{'-' * 30}")
-print(f"{'SSIM Score:':<20} {ssim_score:.4f}")
-
-
-def calculate_psnr(real, fake, max_pixel=1.0):
-    """
-    Calcula el PSNR (Peak Signal-to-Noise Ratio) entre las imágenes reales y generadas.
-    
-    Args:
-    - real (tensor): Imagen real (en tensor).
-    - fake (tensor): Imagen generada (en tensor).
-    - max_pixel (float): El valor máximo posible de los píxeles (por ejemplo, 1.0 para imágenes normalizadas).
-    
-    Returns:
-    - psnr (float): El valor del PSNR.
-    """
-    # Calcular MSE (Mean Squared Error)
+def calculate_psnr(real, fake):
     mse = F.mse_loss(fake, real)
-    
-    # Calcular PSNR a partir del MSE
-    if mse == 0:
-        return 100  # Si el MSE es 0, PSNR es infinito (imágenes idénticas)
-    
-    psnr = 20 * torch.log10(max_pixel / torch.sqrt(mse))
-    return psnr.item()
+    return 20 * torch.log10(1.0 / torch.sqrt(mse)).item() if mse > 0 else 100
 
-# Función de evaluación con PSNR
 def evaluate_psnr(dataloader, netG, device, params):
-    psnr_total = 0
-    num_batches = 0
-    
-    # Evaluar el modelo en lotes
+    psnr_total, num_batches = 0, 0
     with torch.no_grad():
         for real_data, _ in dataloader:
             real_data = real_data.to(device)
-            
-            # Generar datos falsos
             noise = torch.randn(real_data.size(0), params['nz'], 1, 1, device=device)
             fake_data = netG(noise)
-            
-            # Asegurarse de que las imágenes tengan el mismo rango de valores
-            # Normalizar las imágenes entre [0, 1] si es necesario (dependiendo de cómo estén las imágenes)
-            fake_data = fake_data / 2 + 0.5  # Para asegurarse de que las imágenes estén entre [0, 1]
-            real_data = real_data / 2 + 0.5  # Para asegurarse de que las imágenes estén entre [0, 1]
-            
-            # Calcular PSNR para este batch
-            psnr_batch = calculate_psnr(real_data, fake_data)
-            psnr_total += psnr_batch
+            psnr_total += calculate_psnr(real_data / 2 + 0.5, fake_data / 2 + 0.5)
             num_batches += 1
+    return psnr_total / num_batches
+
+def eval_lpips(dataloader, netG, device, imsize):
+
+    lpips_model = lpips.LPIPS(net='vgg').to(device)
+    transform = transforms.Compose([
+        transforms.Resize((imsize, imsize)),
+        transforms.ToTensor()
+    ])
+
+    # Cargar dataset de imágenes reales
+    # adenocarcinoma_left.lower.lobe_T2_N0_M0_Ib"
+    real_dataset = datasets.ImageFolder(root=f"{config['datasets']['chestKaggle']}valid/", transform=transform)
+    real_dataloader = DataLoader(real_dataset, batch_size=1, shuffle=True)
+
+    # Obtener una imagen real del dataset
+    real_image, _ = next(iter(real_dataloader))  
+    real_image = real_image.to(device)
+
+    latent_dim = 100  
+    z = torch.randn(1, latent_dim, 1, 1, device=device)  
+    generated_image = netG(z) 
+
+    # Calcular LPIPS
+    with torch.no_grad():
+        lpips_value = lpips_model(real_image, generated_image).item()
+
+    return lpips_value
+
+
+
+def main(dataset="chestct", model_name="model_ChestCT.pth"):
+    print_green("Evaluating model...")
+    model_path = "model/model_wgan/"
+    config_path = "config.json"
+    with open(config_path, 'r') as json_file:
+        config = json.load(json_file)
+    device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+    print(device, " will be used.\n")
     
-    # Promedio de PSNR sobre todos los lotes
-    average_psnr = psnr_total / num_batches
-    return average_psnr
-
-# Ejemplo de cómo usarlo
-psnr_score = evaluate_psnr(dataloader, netG, device, params)
-# Imprimir --> SSIM 
-print(f"{'-' * 30}")
-print(f"{'PSNR Score:':<20} {psnr_score:.2f} dB")
-print(f"{'-' * 30}")
-
-
-"""
-#############################################  
-                  LPIPS 
- ############################################   
-"""
-# Sólo se puede con ordenadores con GPU
-
-if not torch.cuda.is_available():
-    print("GPU no disponible. No se puede evaluar el modelo con la métrica LPIPS.")
-else:
-    print("GPU disponible. El modelo se ejecutará en la GPU.")
-
-    # Cargar el modelo LPIPS preentrenado (por ejemplo, usando VGG)
-    lpips_model = lpips.LPIPS(net='vgg').cuda()
-
-    # Función para cargar y preprocesar imágenes
-    def load_image(image_path):
-        image = Image.open(image_path).convert('RGB')
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
-        return transform(image).unsqueeze(0).cuda()
-
-    # Cargar imágenes de ejemplo
-    image1 = load_image(f'{image_path}/generated_image_30.png')
-    image2 = load_image('../../../ChestCTKaggle/Data/valid/normal/5.png')
-
-
-    lpips_score = lpips_model(image1, image2)
-    print(f"LPIPS Score: {lpips_score.item():.4f}")
+    # Cargar modelo
+    netG, netD, params = load_model(model_path, device, "wgan", model_name)
+    print_green(str(params))
+    
+    # Obtener el dataloader usando get_chestct
+    if dataset == "chestct":
+        dataloader = get_chestct()
+    else:
+        raise ValueError(f"Unknown dataset type: {dataset}")
 
 
 
+    # Evaluar los modelos
+    accuracy_discriminator, accuracy_generator = evaluate_models(netG, netD, dataloader, device, params)
+    
+    # Evaluar SSIM, PSNR y LPIPS
+    ssim_score = evaluate_ssim(dataloader, netG, device)
+    psnr_score = evaluate_psnr(dataloader, netG, device, params)
+    lpips_score = eval_lpips(dataloader, netG, device, params["imsize"])
+    
+    print(f"{'-' * 30}")
+    print(f"{'Model Evaluation Results':^30}")
+    print(f"{'-' * 30}")
+    print(f"{'Discriminator Accuracy:':<20} {accuracy_discriminator * 100:.2f}%")
+    print(f"{'Generator Accuracy:':<20} {accuracy_generator * 100:.2f}%")
+    print(f"{'SSIM Score:':<20} {ssim_score:.4f}")
+    print(f"{'PSNR Score:':<20} {psnr_score:.4f}")
+    print(f"{'LPIPS Score':<20} {lpips_score:.4f}")
+    print(f"{'-' * 30}")
+
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Evaluate a GAN model")
+    parser.add_argument("--dataset", type=str, default="chestct", choices=["nbia", "chestct"], help="Dataset to use for evaluation")
+    parser.add_argument("--model_name", type=str, default="model_ChestCT.pth", help="Name of the model checkpoint to load")
+    args = parser.parse_args()
+
+
+    main(dataset=args.dataset, model_name=args.model_name)
